dec08dcdb120d69f1b1909fb69670b95650b4dab
Remi1001100 <guju0706@gmail.comq>
2025-08-25 01:24:13 +0900

    [STYLE] 글로벌 스타일 지정

---
+++ src/style/Theme.jsx
@@ -0,0 +1,112 @@
+const theme = {
+  colors: {
+    Primary100: "#00664F",
+    Primary50: "#659788",
+    Primary20: "#B3D4CA",
+    SubColor1: "#1A7E33",
+    SubColor2: "#BBEC57",
+
+    gray1: "#f7f8fa",
+    gray2: "#f2f3f6",
+    gray3: "#eaebee",
+    gray4: "#dcdee3",
+    gray5: "#d1d3d8",
+    gray6: "#adb1ba",
+    gray7: "#868b94",
+    gray8: "#4d5159",
+    gray9: "#393a40",
+    gray10: "#212124",
+
+    white: "#ffffff",
+    blackMain: "#111111",
+  },
+
+  font: {
+    fontSize: {
+      headline20: "20px",
+      headline18: "18px",
+      title15: "15px",
+      body14: "14px",
+      label14: "14px",
+    },
+
+    lineHeight: {
+      normal: "130%",
+      wide: "150%",
+    },
+
+    fontWeight: {
+      bold: 700,
+      semiBold: 600,
+      medium: 500,
+      regular: 400,
+    },
+
+    letterSpacing: {
+      default: "0%",
+      minus: "-1%",
+    },
+  },
+
+  textStyles: {
+    headline1Bold: {
+      fontSize: "20px",
+      lineHeight: "130%",
+      fontWeight: 700,
+      letterSpacing: "0%",
+    },
+    headline2Bold: {
+      fontSize: "18px",
+      lineHeight: "130%",
+      fontWeight: 700,
+      letterSpacing: "0%",
+    },
+
+    titleSemiBold: {
+      fontSize: "15px",
+      lineHeight: "130%",
+      fontWeight: 600,
+      letterSpacing: "0%",
+    },
+
+    body1Regular: {
+      fontSize: "14px",
+      lineHeight: "150%",
+      fontWeight: 400,
+      letterSpacing: "-1%",
+    },
+
+    label1SemiBold: {
+      fontSize: "14px",
+      lineHeight: "130%",
+      fontWeight: 600,
+      letterSpacing: "0%",
+    },
+    label1Medium: {
+      fontSize: "14px",
+      lineHeight: "130%",
+      fontWeight: 500,
+      letterSpacing: "0%",
+    },
+    label2SemiBold: {
+      fontSize: "12px",
+      lineHeight: "130%",
+      fontWeight: 600,
+      letterSpacing: "0%",
+    },
+    label2Medium: {
+      fontSize: "12px",
+      lineHeight: "130%",
+      fontWeight: 500,
+      letterSpacing: "-1%",
+    },
+    label2Regular: {
+      fontSize: "12px",
+      lineHeight: "130%",
+      fontWeight: 500,
+      letterSpacing: "0%",
+    },
+  },
+};
+
+export default theme;
