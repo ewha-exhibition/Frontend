1e7e60c5b3086125b568adf95626d3d834f8c12b
Remi1001100 <guju0706@gmail.com>
2025-09-18 23:39:09 +0900

    [feat] Topbar 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import Scrap from "./pages/Scrap.jsx";
+import CreateReview from "./pages/CreateReview.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/scrap" element={<Scrap />} />
+      <Route path="/createReview" element={<CreateReview />} />
     </Routes>
   );
 }
