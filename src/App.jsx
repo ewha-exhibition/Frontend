b055d008345b4e5b864fcead1dfc1f3684a3f458
Remi1001100 <guju0706@gmail.com>
2025-10-02 21:11:54 +0900

    [feat] 방명록

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Scrap from "./pages/Scrap.jsx";
 import CreateReview from "./pages/CreateReview.jsx";
+import GuestBook from "./pages/guestBook/GuestBook.jsx";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/scrap" element={<Scrap />} />
       <Route path="/createReview" element={<CreateReview />} />
+      <Route path="/guestBook" element={<GuestBook />} />
     </Routes>
   );
 }
---
+++ src/pages/guestBook/Review.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+import ReivewItem from "../../components/guestBook/ReviewItem";
+
+import poster1 from "../../assets/mock/poster1.jpg";
+import poster2 from "../../assets/mock/poster2.jpg";
+import poster3 from "../../assets/mock/poster3.jpg";
+
+function Review() {
+  const mockData = {
+    status: 200,
+    result: [
+      {
+        poster: poster1,
+        title: "2025 조형예술대학 메이데이 전시",
+        id: 1,
+        review:
+          "퀄리티 대박! 너무 알찬 전시 잘 구경하고 갑니다! 무료 굿즈도 너무 감사합니다~ 금손벗들 졸업 축하드려요~  ",
+        pic: [{ src: poster1 }, { src: poster2 }, { src: poster3 }],
+      },
+      {
+        poster: poster2,
+        title: "연극동아리 공연 “너를 걸어가는 시간",
+        id: 2,
+        review:
+          "연극 배우들의 연기가 실감 나서 감동했어요.연극 배우들의 연기가 실감 나서 감동했어요.연극 배우들의 연기가 실감 나서 감동했어요.연극 배우들의 연기가 실감 나서 감동했어요.연극 배우들의 연기가 실감 나서 감동했어요.연극 배우들의 연기가 실감 나서 감동했어요.",
+        pic: [],
+      },
+      {
+        poster: poster3,
+        title: "2025 조형예술대학 메이데이 전시",
+        id: 3,
+        review:
+          "퀄리티 대박! 너무 알찬 전시 잘 구경하고 갑니다! 무료 굿즈도 너무 감사합니다~ 금손벗들 졸업 축하드려요~ ",
+        pic: [
+          { src: poster1 },
+          { src: poster2 },
+          { src: poster3 },
+          { src: poster3 },
+        ],
+      },
+      {
+        poster: poster3,
+        title: "2025 조형예술대학 메이데이 전시",
+        id: 4,
+        review:
+          "퀄리티 대박! 너무 알찬 전시 잘 구경하고 갑니다! 무료 굿즈도 너무 감사합니다~ 금손벗들 졸업 축하드려요~ ",
+        pic: [{ src: poster1 }],
+      },
+            {
+        poster: poster1,
+        title: "2025 조형예술대학 메이데이 전시",
+        id: 5,
+        review:
+          "퀄리티 대박! 너무 알찬 전시 잘 구경하고 갑니다! 무료 굿즈도 너무 감사합니다~ 금손벗들 졸업 축하드려요~ ",
+        pic: [
+          { src: poster1 },
+          { src: poster2 },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <Container>
+      {mockData?.result.map((data) => (
+        <ReivewItem
+          key={data.id}
+          poster={data.poster}
+          title={data.title}
+          id={data.id}
+          review={data.review}
+          pic={data.pic}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default Review;
+
+const Container = styled.div``;
