e0d6ee55638d2c13f7fe07419d1f141a6da36bcb
Remi1001100 <guju0706@gmail.comq>
2025-10-11 15:27:34 +0900

    [feat] 관람 내역 컴포넌트

--- src/pages/myPage/MyPage.jsx
+++ src/pages/myPage/MyPage.jsx
@@ -8,7 +8,63 @@ import Banner from "../../components/myPage/Banner";
 import Kakao from "../../assets/icons/Kakao.png";
 import ChevronRight from "../../assets/icons/ChevronRight.svg?react";
 
+import poster1 from "../../assets/mock/poster1.jpg";
+import poster2 from "../../assets/mock/poster2.jpg";
+import poster3 from "../../assets/mock/poster3.jpg";
+import poster4 from "../../assets/mock/poster4.jpg";
+
 function MyPage() {
+  const mock_data = {
+    response: 200,
+    result: [
+      {
+        id: 1,
+        title: "Pile up strands - 섬유예술 전공 과제전시회 어쩌고저쩌고 텍스트",
+        date: "2025.11.20-12.01",
+        place: "이화여대 조형예술관 A동  2,4층",
+        poster: poster1,
+        haveReview: false,
+        onGoing: true,
+      },
+      {
+        id: 2,
+        title: "Pile up strands - 섬유예술 전공 과제전시회",
+        date: "2025.09.10-09.11",
+        place: "이화여대 조형예술관 A동  2,4층",
+        poster: poster2,
+        haveReview: true,
+        onGoing: false,
+      },
+      {
+        id: 3,
+        title: "Pile up strands - 섬유예술 전공 과제전시회",
+        date: "2025.11.20-12.01",
+        place: "이화여대 조형예술관 A동  2,4층",
+        poster: poster3,
+        haveReview: true,
+        onGoing: true,
+      },
+      {
+        id: 4,
+        title: "Pile up strands - 섬유예술 전공 과제전시회",
+        date: "2025.11.20-12.01",
+        place: "이화여대 조형예술관 A동  2,4층",
+        poster: poster4,
+        haveReview: false,
+        onGoing: false,
+      },
+      {
+        id: 5,
+        title: "Pile up strands - 섬유예술 전공 과제전시회",
+        date: "2025.11.20-12.01",
+        place: "이화여대 조형예술관 A동  2,4층",
+        poster: poster4,
+        haveReview: false,
+        onGoing: false,
+      },
+    ],
+  };
+
   const theme = useTheme();
   const [login, setLogin] = useState(false);
   //disabled 하는 기능 필요
@@ -53,21 +109,32 @@ function MyPage() {
           <TabList>
             {firstTabs.map((tab) => (
               <div key={tab.path}>
-                <TabItem
-                  key={tab.path}
-                  onClick={() => navigate(`/mypage/${tab.path}`)}
-                >
+                <TabItem onClick={() => navigate(`/mypage/${tab.path}`)}>
                   {tab.name}
                   <ChevronRight height={24} width={24} />
                 </TabItem>
+
                 {login && tab.path === "watched" && (
-                  <History>
-                    <p className="notice">아직 관람 내역이 없어요.</p>
-                    <p className="notice">
-                      관람한 행사가 있다면 후기를 작성하거나 스크랩 목록에서
-                      “관람했어요” 버튼을 눌러주세요.
-                    </p>
-                  </History>
+                  <>
+                    {mock_data.response === 200 ? (
+                      <ShowListArea>
+                        {mock_data?.result?.map((data) => (
+                          <ShowList key={data.id}>
+                            <img src={data.poster} alt="포스터 이미지" />
+                            <p>{data.title}</p>
+                          </ShowList>
+                        ))}
+                      </ShowListArea>
+                    ) : (
+                      <History>
+                        <p className="notice">아직 관람 내역이 없어요.</p>
+                        <p className="notice">
+                          관람한 행사가 있다면 후기를 작성하거나 스크랩 목록에서
+                          “관람했어요” 버튼을 눌러주세요.
+                        </p>
+                      </History>
+                    )}
+                  </>
                 )}
               </div>
             ))}
@@ -100,12 +167,12 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.gray1};
 `;
 const Content = styled.div`
-  padding: 0px 20px 0px 20px;
+  //padding: 0px 20px 0px 20px;
 `;
 const Login = styled.div`
   display: flex;
   gap: 10px;
-  padding: 40px 0;
+  padding: 40px 20px 0 20px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -149,6 +216,7 @@ const TabList = styled.div`
   margin: 20px 0;
 `;
 const TabItem = styled.div`
+  padding: 0px 20px 0px 20px;
   height: 24px;
   display: flex;
   align-items: center;
@@ -164,6 +232,8 @@ const Br = styled.div`
   margin: 24px 0;
 `;
 const SubTitle = styled.div`
+  padding: 0px 20px 0px 20px;
+
   color: ${({ theme }) => theme.colors.Primary60};
   font-size: ${({ theme }) => theme.font.fontSize.label16};
   font-weight: ${({ theme }) => theme.font.fontWeight.semiBold};
@@ -189,5 +259,37 @@ const History = styled.div`
     line-height: ${({ theme }) => theme.font.lineHeight.normal};
     text-align: center;
     max-width: 270px;
+  }
+`;
+const ShowListArea = styled.div`
+  padding: 12px 0 12px 20px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  overflow-x: auto;
+`;
+const ShowList = styled.div`
+  max-width: 93px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  img {
+    height: 130px;
+    aspect-ratio: 46/65;
+    border-radius: 3px;
+    background: ${({ theme }) => theme.colors.gray2};
+  }
+  p {
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    color: ${({ theme }) => theme.colors.gray9};
+    font-size: ${({ theme }) => theme.font.fontSize.label12};
+    font-weight: ${({ theme }) => theme.font.fontWeight.semiBold};
+    line-height: ${({ theme }) => theme.font.lineHeight.normal};
   }
 `;
