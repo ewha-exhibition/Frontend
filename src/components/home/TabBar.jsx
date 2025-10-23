df8c6a14c23c108b9d98bf1b54eeee18c1782634
KimDajee <daisy.k@ewha.ac.kr>
2025-10-24 00:46:04 +0900

    [SCRUM-46] 하단 네비게이션 연결 및 SVG, 홈화면 리펙토링

--- src/components/home/TabBar.jsx
+++ src/components/home/TabBar.jsx
@@ -1,27 +1,40 @@
 import styled from "styled-components";
-import theme from "../../style/Theme.jsx";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import HomeIcon from "../../assets/icons/HomeIcon.svg?react";
 import ScrapIcon from "../../assets/icons/Bookmark.svg?react";
 import GuestBookIcon from "../../assets/icons/GuestBook.svg?react";
 import MyPageIcon from "../../assets/icons/MyPage.svg?react";
 
-//TODO: active tab state 관리 필요
 export default function TabBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const tabs = [
-    { key: "home", label: "홈", Icon: HomeIcon },
-    { key: "scrap", label: "스크랩", Icon: ScrapIcon },
-    { key: "guestbook", label: "방명록", Icon: GuestBookIcon },
-    { key: "my", label: "마이", Icon: MyPageIcon },
+    { key: "home", label: "홈", Icon: HomeIcon, path: "/" },
+    { key: "scrap", label: "스크랩", Icon: ScrapIcon, path: "/scrap" },
+    {
+      key: "guestbook",
+      label: "방명록",
+      Icon: GuestBookIcon,
+      path: "/guestBook",
+    },
+    { key: "my", label: "마이", Icon: MyPageIcon, path: "/mypage" },
   ];
 
   return (
     <NavBar>
-      {tabs.map(({ key, label, Icon }) => (
-        <NavItem key={key}>
-          <Icon width={24} height={24} />
-          <p>{label}</p>
-        </NavItem>
-      ))}
+      {tabs.map(({ key, label, Icon, path }) => {
+        // 현재 페이지와 경로 일치 확인
+        const isActive = location.pathname === path;
+
+        return (
+          <NavItem key={key} $active={isActive} onClick={() => navigate(path)}>
+            <StyledIcon $active={isActive} as={Icon} alt={label} />
+            <p className="label">{label}</p>
+          </NavItem>
+        );
+      })}
     </NavBar>
   );
 }
@@ -34,11 +47,11 @@ const NavBar = styled.div`
   z-index: 100;
 
   height: 50px;
-  background: #fff;
+  background: ${({ theme }) => theme.colors.white};
   display: flex;
   justify-content: space-around;
   align-items: center;
-  border-top: 0.5px solid ${theme.colors.gray5};
+  border-top: 0.5px solid ${({ theme }) => theme.colors.gray5};
 `;
 
 const NavItem = styled.div`
@@ -47,17 +60,23 @@ const NavItem = styled.div`
   align-items: center;
   padding: 4px 0;
   gap: 1px;
-  color: ${theme.colors.gray5};
-  p {
+  color: ${({ theme }) => theme.colors.gray5};
+
+  .label {
+    color: ${({ theme }) => theme.colors.gray5};
     font-size: 11px;
     text-align: center;
     font-family: SUIT;
     font-weight: 500;
     line-height: 127.3%;
     letter-spacing: 0.11px;
+    color: ${({ $active, theme }) =>
+      $active ? theme.colors.gray10 : theme.colors.gray5};
   }
-
-  svg {
-    fill: ${theme.colors.gray5};
-  }
+`;
+const StyledIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+  color: ${({ $active, theme }) =>
+    $active ? theme.colors.gray10 : theme.colors.gray5};
 `;
