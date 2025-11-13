48bb8ec4db8eb846b494cc8f48572abdc56264b9
Remi1001100 <guju0706@gmail.com>
2025-11-13 23:35:02 +0900

    feat(SRCUM-61): api hook 제작 및 스크랩 페이지 연결

--- src/components/Scraped.jsx
+++ src/components/Scraped.jsx
@@ -1,9 +1,62 @@
+import useCustomFetch from "../utils/hooks/useCustomFetch";
+
 import styled from "styled-components";
 import Bookmark from "../assets/icons/Bookmark.svg?react";
 import BookmarkOL from "../assets/icons/BookmarkOL.svg?react";
 import HaveSeen from "./buttons/HaveSeen";
 
-function Scraped({ id, title, date, place, poster, onGoing, scraped }) {
+function Scraped({
+  id,
+  title,
+  startDate,
+  endDate,
+  place,
+  poster,
+  scraped,
+  onDelete,
+}) {
+  function formatDate(dateString) {
+    return dateString.replace(/-/g, ".");
+  }
+
+  const formatStartDate = formatDate(startDate);
+  const formatEndDate = formatDate(endDate);
+
+  function checkOnGoing(endDate) {
+    const today = new Date();
+    const target = new Date(endDate);
+    return today <= target;
+  }
+
+  const onGoing = checkOnGoing(endDate);
+  console.log(onGoing);
+
+  function useDeleteBookmark(refetchScraps) {
+    const { fetchData } = useCustomFetch();
+
+    const deleteBookmark = useCallback(
+      async (id) => {
+        try {
+          const response = await fetchData(`/scraps/${id}`, "DELETE", null);
+
+          if (response?.status === 200) {
+            console.log("북마크 삭제 완료");
+            if (refetchScraps) {
+              await refetchScraps();
+            }
+          } else {
+            console.error("삭제 실패:", response);
+          }
+        } catch (error) {
+          console.error("북마크 삭제 중 오류:", error);
+        }
+      },
+      [fetchData, refetchScraps]
+    );
+
+    return { deleteBookmark };
+  }
+
   return (
     <Component>
       <Container>
@@ -11,13 +64,21 @@ function Scraped({ id, title, date, place, poster, onGoing, scraped }) {
         <TextArea>
           <div className="titleBox">{title}</div>
           <p>{place}</p>
-          <p>{date}</p>
+          <p>
+            {formatStartDate}~{formatEndDate}
+          </p>
         </TextArea>
-        {scraped ? (
-            <GreenBookmark width={24} height={24} />
+        <BookmarkWrapper>
+          {scraped ? (
+            <GreenBookmark
+              width={24}
+              height={24}
+              onClick={() => onDelete(id)}
+            />
           ) : (
             <BookmarkOL width={24} height={24} />
-        )}
+          )}
+        </BookmarkWrapper>
 
         <BtnArea>
           <HaveSeen />
@@ -32,7 +93,7 @@ export default Scraped;
 
 const GreenBookmark = styled(Bookmark)`
   color: ${({ theme }) => theme.colors.Primary50};
-`
+`;
 const Component = styled.div`
   width: 100%;
   display: flex;
@@ -65,15 +126,15 @@ const TextArea = styled.div`
   flex-direction: column;
 
   .titleBox {
-    max-width: 190px;
-    
+    width: 190px;
+
     color: ${({ theme }) => theme.colors.gray10};
     font-size: ${({ theme }) => theme.font.fontSize.title15};
     font-weight: ${({ theme }) => theme.font.fontWeight.semiBold};
     line-height: ${({ theme }) => theme.font.lineHeight.normal};
 
     display: -webkit-box;
-    -webkit-line-clamp: 2; 
+    -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -87,6 +148,12 @@ const TextArea = styled.div`
     line-height: ${({ theme }) => theme.font.lineHeight.normal};
   }
 `;
+const BookmarkWrapper = styled.div`
+  position: absolute;
+  top: 12px;
+  right: 0;
+`;
+
 const BtnArea = styled.div`
   position: absolute;
   right: 0px;
