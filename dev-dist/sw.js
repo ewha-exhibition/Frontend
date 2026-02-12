8ee7ae25753cb20c7b83070caad410490d0ce47e
Remi1001100 <guju0706@gmail.comq>
2026-02-12 17:20:32 +0900

    modify(SCRUM-87): 스크랩 모달 오류 수정, 관람 api 적용

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b4s08illpbo"
+    "revision": "0.dmlc3qnn5g8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
